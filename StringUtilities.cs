8278a615adb6b798bede5c281e78d6721245bac6
Meeran Mydeen S <meeran@buckman.com>
2024-12-26 13:05:39 +0530

    first commit

---
+++ StringUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleClassLibrary
+{
+    public static class StringUtilities
+    {
+        public static bool IsPalindrome(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            var reversed = new string(input.Reverse().ToArray());
+            return string.Equals(input, reversed, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static string ToCamelCase(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return input;
+
+            return char.ToLower(input[0]) + input.Substring(1);
+        }
+    }
+}
